fdc3ac38b1e9bd08b785f80526bb8c440e38dfe8
JR <jadedResearcher@gmail.com>
2022-08-03 13:02:41 -0400

    updates

---
+++ src/Secrets/Content/31.js
@@ -0,0 +1,23 @@
+export const text = `
+* JR NOTE: PLEASE KEEP IN MIND THAT DOC SLAUGHTER IS FROM ANOTHER (MORE PARANOID) UNIVERSE, AND THAT THOSE WRITING HER ARE NOT ACTUALLY LICENSED PSYCHOTHERAPISTS. DO NOT TAKE ANY OF HER OPINIONS AS FACTS. 
+
+Name: Captain
+Aliases:  The Reflected, L-0-I1-beta
+Coping Strategy: Wounded and Defensive (Control)
+Attachment Style: Pending
+
+Quick Summary:
+
+
+The Captain is a study in contrasts. A man who revels in his physical prowess (especially for his age), he equally seems to feel helpless in the face of Societal Expectations. Observing Yongki's unique lack of response to those Expectations has proven Illuminating for him.
+
+The Captain remains tight-lipped about certain aspects of his upbringing, but it seems clear he comes from a strict background. He expects rules to be clearly defined, and for everyone to follow them.  Deviations from rules (real or imagined) causes him great distress and results in attempts to control those around him in the same manner he would control himself.
+
+As a result, his return to his former co-workers has resulted in distress and a retreat to rules. He is bewildered at the various changes in those who should be familiar to him. He is further caught off guard that when he finally returned to his body, it was in an entirely new, strange universe. The phrase "you can't go home again" seems especially relevant.
+
+I have been working with the Captain to allow more leeway in "roommate" agreements with Yongki, as well as hinting that perhaps group therapy would be appropriate for the Information team more broadly. His return has certainly destabilized certain dynamics in ways that could be leveraged to obtain real Growth for all.
+
+However, Significant Challenges remain blocking this option, namely Captain's inability to control Yongki's severe physical response to danger or aggression.  He has taken to the challenge with aplomb, providing the Hypothesis that Yongki's more lackadaisical nature may result in superior control of one's body.  I am working with him to find ways to evaluate this Hypothesis and provide regimens for increasing control.
+
+
+`;
