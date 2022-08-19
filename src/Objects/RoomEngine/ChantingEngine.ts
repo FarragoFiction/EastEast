d1871818f937a93769f5a3d2309cd94fb99f51fc
JR <jadedResearcher@gmail.com>
2022-08-18 21:16:45 -0400

    thing

--- src/Objects/RoomEngine/ChantingEngine.ts
+++ src/Objects/RoomEngine/ChantingEngine.ts
@@ -12,7 +12,7 @@ export class ChantingEngine {
     //JR NOTE: todo , still raw audio, needs cleanup
     //the loops is not a loop
     sources = ["Drone1.mp3", "Drone2Fractal.mp3", "Drone3.mp3"];
-    audio = new Audio(this.baseLocation + this.sources[2]);
+    audio = new Audio(this.baseLocation + this.sources[1]);
     tickNum = 0;
     volumeDirection = Direction.UP;
 
