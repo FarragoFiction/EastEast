7d4464904e484b2a9610156c08657a5aa70f7079
JR <jadedResearcher@gmail.com>
2022-08-23 19:57:53 -0400

    add and remove themes from objects

---
+++ src/Objects/Entities/Actions/AddThemeToObject.ts
@@ -0,0 +1,35 @@
+
+import { Action } from "./BaseAction";
+
+import { AiBeat } from "../StoryBeats/BaseBeat";
+import { MoveToSpecificPhysicalObject } from "../../MovementAlgs/MoveToSpecificPhysicalObject";
+import { Room } from "../../RoomEngine/Room";
+import { Theme } from "../../Theme";
+
+export class AddThemeToObject extends Action{ //lawsuit
+    theme: Theme; //technically storing the key would be a smaller footprint but then i'd have to wait till runtime to find out if i typoed a key and boy do i not want to do that rn
+    
+    recognizedCommands:string[] =[]
+
+    constructor(theme:Theme){
+        super();
+        this.theme = theme;
+    }
+
+    applyAction = (beat: AiBeat)=>{
+        const subject = beat.owner;
+        if(!subject){
+            return "";
+        }
+
+        const target = beat.targets;
+        if(target.length < 1){
+            return `${subject.processedName()} can't see anything to modify with ${this.theme.key}...`;
+        }
+        subject.themes.push(this.theme);
+        return `${subject.processedName()} modifies the  ${target[0].processedName()} to be more ${this.theme.key}.`;
+    }
+
+
+
+}
