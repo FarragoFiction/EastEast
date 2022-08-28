0b87fc3b5e2d947d17dccbbb3105e73ea2c2d20f
JR <jadedResearcher@gmail.com>
2022-08-27 23:09:23 -0400

    people have to be objects or chickens can't eat taylor from worm

--- src/Objects/Entities/TargetFilter/TargetIsNearObjectWithTheme.ts
+++ src/Objects/Entities/TargetFilter/TargetIsNearObjectWithTheme.ts
@@ -1,38 +1,39 @@
 import { turnArrayIntoHumanSentence } from "../../../Utils/ArrayUtils";
 import { PhysicalObject } from "../../PhysicalObject";
+import { Theme } from "../../Theme";
 import { AiBeat } from "../StoryBeats/BaseBeat";
 import { TargetFilter } from "./baseFilter";
 
-//used for things like "if target is killer && target is near egg";
-export class TargetNearObjectWithName extends TargetFilter {
-    words: string[];
+//used for things like "if target is near a plant";
+export class TargetNearObjectWithTheme extends TargetFilter {
+    themes: Theme[];
     //NOTE NO REAL TIME INFRMATION SHOULD BE STORED HERE. ANY INSTANCE OF THIS FILTER SHOULD BEHAVE THE EXACT SAME WAY
 
 
-    constructor(words: string[], singleTarget = false, invert = false, kMode = false) {
+    constructor(themes: Theme[], singleTarget = false, invert = false, kMode = false) {
         super(singleTarget, invert, kMode);
-        this.words = words;
+        this.themes = themes;
     }
 
     toString = () => {
         //format this like it might start with either because or and
-        if (this.words.length === 1) {
-            return `they see something near something named ${this.words[0]}`;
+        if (this.themes.length === 1) {
+            return `they see something near something associated with ${this.themes[0].key}`;
         }
-        return `they see something near an object named any of these words ${turnArrayIntoHumanSentence(this.words)}`;
+        return `they see something near an object associated with any of these themes ${turnArrayIntoHumanSentence(this.themes.map((i)=>i.key))}`;
     }
 
     applyFilterToSingleTarget = (owner: AiBeat, target: PhysicalObject) => {
         let targetLocked = false;
-        for (let word of this.words) {
+        for (let theme of this.themes) {
             for (let item of target.room.items){
-                if (item.processedName().toUpperCase().includes(word.toUpperCase())) {
+                if (item.themes.includes(theme)) {
                     targetLocked = true;
                 }
             }
 
             for (let item of target.room.blorbos){
-                if (item.processedName().toUpperCase().includes(word.toUpperCase())) {
+                if (item.themes.includes(theme)) {
                     targetLocked = true;
                 }
             }
