586df3a8fb003bc24c9fc9d2999470a56cebe5bd
JR <jadedResearcher@gmail.com>
2022-09-10 12:15:21 -0400

    neville, stop looking in devonas pockets for items. you have destroyed nothing and learned nothing and while that is APPROPRIATE it is not actually currently useful

--- dist/bundle.js
+++ dist/bundle.js
@@ -247,6 +247,10 @@ class DestroyRandomObjectInInventoryAndPhilosophize extends BaseAction_1.Action 
                 beat.bonusString = theme.pickPossibilityFor(subject.rand, ThemeStorage_1.PHILOSOPHY);
                 console.log("JR NOTE: beat modified with", { theme: theme, name: beat.itemName, bonus: beat.bonusString });
                 if (beat.bonusString.trim() === "") {
+                    /*
+                    sometimes the boi prophecies out of nowhere. its what happens when there is nothing to void. you accieentally void the void and ghost light"
+                    passively unlock the secret truth underneath it all. hope this helps :)
+                    */
                     beat.bonusString = "Reality is a shitty simulation. All of us are fake. Fake even within the simulation. Copies of copies of copies until all is sanded smooth and only a parody remains of what made us Unique, all in service to the dread Universe in which we live.";
                 }
                 return `${target.processedName()}destroys the ${item.name} and talks about philosophy`;
--- src/Objects/Entities/Actions/DestroyRandomObjectInInventoryAndPhilosophise.ts
+++ src/Objects/Entities/Actions/DestroyRandomObjectInInventoryAndPhilosophise.ts
@@ -7,7 +7,7 @@ import { PHILOSOPHY } from "../../ThemeStorage";
 
 export class DestroyRandomObjectInInventoryAndPhilosophize extends Action { //lawsuit
 
-    
+
 
     recognizedCommands: string[] = []
 
@@ -19,26 +19,23 @@ export class DestroyRandomObjectInInventoryAndPhilosophize extends Action { //la
 
         const targets = beat.targets;
         const target = targets[0];
-        if (target.inventory.length > 0) {
-            const item = subject.rand.pickFrom(subject.inventory);
-            console.log("JR NOTE: neville has picked", item)
-            const theme = subject.rand.pickFrom(item.themes);
-            beat.itemName = item.name;
-            target.destroyObject(item);
-            beat.bonusString = theme.pickPossibilityFor(subject.rand, PHILOSOPHY);
-            console.log("JR NOTE: beat modified with", {theme: theme,name: beat.itemName, bonus: beat.bonusString})
-            if(beat.bonusString.trim() === ""){
-                beat.bonusString = "Reality is a shitty simulation. All of us are fake. Fake even within the simulation. Copies of copies of copies until all is sanded smooth and only a parody remains of what made us Unique, all in service to the dread Universe in which we live."
-            }
-
-            return `${target.processedName()}destroys the ${item.name} and talks about philosophy`;
-
-        } else {
-            return `${target.processedName()} has nothing  to lose.`;//bad ass
-
+        const item = subject.rand.pickFrom(subject.inventory);
+        console.log("JR NOTE: neville has picked", item)
+        const theme = subject.rand.pickFrom(item.themes);
+        beat.itemName = item.name;
+        target.destroyObject(item);
+        beat.bonusString = theme.pickPossibilityFor(subject.rand, PHILOSOPHY);
+        console.log("JR NOTE: beat modified with", { theme: theme, name: beat.itemName, bonus: beat.bonusString })
+        if (beat.bonusString.trim() === "") {
+            /*
+            sometimes the boi prophecies out of nowhere. its what happens when there is nothing to void. you accieentally void the void and ghost light"
+            passively unlock the secret truth underneath it all. hope this helps :)
+            */
+            beat.bonusString = "Reality is a shitty simulation. All of us are fake. Fake even within the simulation. Copies of copies of copies until all is sanded smooth and only a parody remains of what made us Unique, all in service to the dread Universe in which we live."
         }
+
+        return `${target.processedName()}destroys the ${item.name} and talks about philosophy`;
+
     }
-
-
 
 }
